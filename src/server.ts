832297e21e097e001169b0f505ea189367a49a71
eyobedk <eyobedekebede10@gmail.com>
2023-07-07 15:35:55 +0300

    init

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import express, { NextFunction, Request, Response } from 'express'
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors())
+
+app.get('/api/v1/user', (req: Request, res: Response, next: NextFunction)=>{
+    try{
+        const user = {
+            id: 1,
+            name: "eyobed",
+            age: 23,
+            blood:"O+"
+        }
+        console.log(user)
+
+        res.status(200).json({
+            data:{
+                user
+            }
+        })
+    }catch(error){throw error}
+})
+
+app.listen(4000, ()=>{
+    console.log('app listening on port ', 4000)
+})
