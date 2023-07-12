571e4374dabf0616c1a4da00ed026602b21d68ff
eyobedk <eyobedekebede10@gmail.com>
2023-07-12 12:43:36 +0300

    added dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express, { NextFunction, Request, Response } from 'express'
 import cors from 'cors';
+import config from 'dotenv';
+config.config({path:'./config.env'})
 
 const app = express();
 
@@ -14,17 +16,15 @@ app.get('/api/v1/user', (req: Request, res: Response, next: NextFunction)=>{
             age: 23,
             blood:"O+"
         }
-        console.log("api work")
 
         res.status(200).json({
-            message:"data sent",
+            message:process.env.ENVIROMENT,
             data:{
                 user
             }
         })
     }catch(error){throw error}
 })
-console.log("here")
 
 app.listen(4000, ()=>{
     console.log('app listening on port ', 4000)
